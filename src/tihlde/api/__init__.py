0d209b60516a9f45adecaf5d68afc2b9920f9b90
Mads Nylund <madsnyl@gmail.com>
2024-08-07 23:49:36 +0200

    fixed imports

--- src/tihlde/api/__init__.py
+++ src/tihlde/api/__init__.py
@@ -1,4 +1,6 @@
-from .user.auth import authenticate
-from .user.me import getMe
-from .user.photo import allowPhoto
+from tihlde.api.users import (
+    authenticate,
+    getMe,
+    allowPhoto
+)
 from tihlde.api.event import allowPhotoByEvent
---
+++ src/tihlde/api/users/__init__.py
@@ -0,0 +1,3 @@
+from tihlde.api.users.auth import authenticate
+from tihlde.api.users.me import getMe
+from tihlde.api.users.photo import allowPhoto
